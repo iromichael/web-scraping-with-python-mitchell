6a80a8478ac0eb94d73b2b49d43ac6e18dd53265
Onyekachi Iroegbu <onyekachiiroegbu@Onyekachis-MacBook-Pro.local>
2026-07-27 09:42:57 +0300

    Add ch01 example: getTitle() with try/except for HTTPError

--- ch04-your-first-web-scraper/scrapetest.py
+++ ch04-your-first-web-scraper/scrapetest.py
@@ -1,4 +1,35 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+from urllib.error import URLError
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+# try:
+#     html = urlopen("http://pythonscraping.com/pages/page1.html")
+# except HTTPError as e:
+#     print(e)
+# except URLError as e:
+#     print("The server could not be found!")
+# else:
+#     print("It worked")
+
+#     bs = BeautifulSoup(html, 'html.parser')
+#     print(bs)
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title is None:
+    print('Title could not be found')
+else:
+    print(title)
+
